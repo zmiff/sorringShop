d9a20d0facb01c4e0fb7a90cac23b75afcaa6416
Ugur Tamer <ugt@brunata.com>
2018-08-16 20:35:50 +0200

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ugt\google~1\webdev\python\projects\sorrin~2\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
